a542123fb8ebdd7b1669353e78608a91b26f67eb
dariayudina <dashaudina06@gmail.com>
2023-10-05 21:34:21 +0400

    parser optimization

--- WeatherParser/Program.cs
+++ WeatherParser/Program.cs
@@ -2,33 +2,33 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
 
 namespace WeatherParser
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             SetEncoding();
-            var cityWeatherData = ParseCitiesWeather();
+            var cityWeatherData = await ParseCitiesWeatherAsync();
             PrintCityWeather(cityWeatherData);
         }
 
-        public static Dictionary<string, List<WeatherEntry>> ParseCitiesWeather()
+        public static async Task<List<KeyValuePair<string, List<WeatherEntry>>>> ParseCitiesWeatherAsync()
         {
-            var cityWeatherData = new Dictionary<string, List<WeatherEntry>>();
+            var cityWeatherData = new List<KeyValuePair<string, List<WeatherEntry>>>();
 
             try
             {
-                var web = new HtmlWeb
-                {
-                    AutoDetectEncoding = false,
-                    OverrideEncoding = Encoding.UTF8,
-                };
+                var httpClient = new HttpClient();
                 var site = "https://www.gismeteo.ru";
-                var doc = web.Load(site);
+                var html = await httpClient.GetStringAsync(site);
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
 
                 var popularCitiesNode = doc.DocumentNode.SelectSingleNode("//div[@class='list']");
                 if (popularCitiesNode != null)
@@ -36,24 +36,24 @@ namespace WeatherParser
                     var cityLinks = popularCitiesNode.SelectNodes(".//a[@class='link']");
                     if (cityLinks != null)
                     {
-                        foreach (var cityLink in cityLinks)
+                        var cityWeatherTasks = cityLinks.Select(async cityLink =>
                         {
                             var cityName = cityLink.InnerText.Trim();
                             var cityUrl = site + cityLink.GetAttributeValue("href", "");
-
-                            // Переход на страницу с погодой за 10 дней
                             var cityWeatherUrl = cityUrl + "10-days/";
-                            var weatherData = ParseCityWeather(web, cityWeatherUrl);
+                            var weatherData = await ParseCityWeatherAsync(httpClient, cityWeatherUrl);
 
                             if (weatherData != null && weatherData.Any())
                             {
-                                cityWeatherData[cityName] = weatherData;
+                                cityWeatherData.Add(new KeyValuePair<string, List<WeatherEntry>>(cityName, weatherData));
                             }
                             else
                             {
                                 Console.WriteLine($"No weather data found for {cityName}");
                             }
-                        }
+                        });
+
+                        await Task.WhenAll(cityWeatherTasks);
                     }
                 }
                 else
@@ -69,47 +69,48 @@ namespace WeatherParser
             return cityWeatherData;
         }
 
-        public static List<WeatherEntry> ParseCityWeather(HtmlWeb web, string cityWeatherUrl)
+        public static async Task<List<WeatherEntry>> ParseCityWeatherAsync(HttpClient httpClient, string cityWeatherUrl)
         {
-            var cityDoc = web.Load(cityWeatherUrl);
             var weatherEntries = new List<WeatherEntry>();
 
-            var dateContainers = cityDoc.DocumentNode.SelectNodes("//div[@class='widget-row widget-row-days-date']//div[@class='date']");
-
-            if (dateContainers != null)
+            try
             {
-                int year = DateTime.Now.Year;
-                int formatMonth = 0;
+                var html = await httpClient.GetStringAsync(cityWeatherUrl);
+                var cityDoc = new HtmlDocument();
+                cityDoc.LoadHtml(html);
 
-                foreach (var dateContainer in dateContainers)
+                var dateContainers = cityDoc.DocumentNode.SelectNodes("//div[@class='widget-row widget-row-days-date']//div[@class='date']");
+
+                if (dateContainers != null)
                 {
-                    var dateText = dateContainer.InnerText.Trim();
-                    var dateParts = dateText.Split(' ');
+                    int year = DateTime.Now.Year;
+                    int formatMonth = 0;
 
-                    if (dateParts.Length >= 1)
+                    foreach (var dateContainer in dateContainers)
                     {
-                        string day = dateParts[0];
+                        var dateText = dateContainer.InnerText.Trim();
+                        var dateParts = dateText.Split(' ');
 
-                        if (dateParts.Length == 2)
+                        if (dateParts.Length >= 1)
                         {
-                            string month = dateParts[1];
-                            formatMonth = FormatDate(month);
-                        }
+                            string day = dateParts[0];
 
-                        string date = $"{day:D2} {formatMonth:D2} {year}";
-
-                        if (DateTime.TryParseExact(date, "d MM yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
-                        {
-                            // Извлечь элементы с классом "value style_size_m"
-                            var temperatureElements = cityDoc.DocumentNode.SelectNodes("//div[@class='value style_size_m']");
-
-                            if (temperatureElements != null)
+                            if (dateParts.Length == 2)
                             {
-                                foreach (var element in temperatureElements)
+                                string month = dateParts[1];
+                                formatMonth = FormatDate(month);
+                            }
+
+                            string date = $"{day:D2} {formatMonth:D2} {year}";
+
+                            if (DateTime.TryParseExact(date, "d MM yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+                            {
+                                var temperatureElements = cityDoc.DocumentNode.SelectNodes("//div[@class='value style_size_m']");
+
+                                if (temperatureElements != null)
                                 {
-                                    // Извлечь значения максимальной и минимальной температуры
-                                    var maxTemperatureElement = element.SelectSingleNode(".//div[@class='maxt']");
-                                    var minTemperatureElement = element.SelectSingleNode(".//div[@class='mint']");
+                                    var maxTemperatureElement = temperatureElements.FirstOrDefault(e => e.SelectSingleNode(".//div[@class='maxt']") != null);
+                                    var minTemperatureElement = temperatureElements.FirstOrDefault(e => e.SelectSingleNode(".//div[@class='mint']") != null);
 
                                     if (maxTemperatureElement != null && minTemperatureElement != null)
                                     {
@@ -128,18 +129,21 @@ namespace WeatherParser
                                     }
                                 }
                             }
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Failed to parse date: {date}");
+                            else
+                            {
+                                Console.WriteLine($"Failed to parse date: {date}");
+                            }
                         }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
 
             return weatherEntries;
         }
-
 
         public static string GetTemperature(HtmlNode temperatureElement)
         {
@@ -186,12 +190,12 @@ namespace WeatherParser
             return -1;
         }
 
-        public static void PrintCityWeather(Dictionary<string, List<WeatherEntry>> cityWeatherData)
+        public static void PrintCityWeather(List<KeyValuePair<string, List<WeatherEntry>>> cityWeatherData)
         {
-            foreach (var cityName in cityWeatherData.Keys)
+            foreach (var cityWeatherPair in cityWeatherData)
             {
-                Console.WriteLine($"City: {cityName}");
-                var weatherData = cityWeatherData[cityName];
+                Console.WriteLine($"City: {cityWeatherPair.Key}");
+                var weatherData = cityWeatherPair.Value;
                 foreach (var weatherEntry in weatherData)
                 {
                     Console.WriteLine($"Date: {weatherEntry.Date}, Max Temperature: {weatherEntry.MaxTemperature}, Min Temperature: {weatherEntry.MinTemperature}");
@@ -203,12 +207,5 @@ namespace WeatherParser
         {
             Console.OutputEncoding = Encoding.UTF8;
         }
-    }
-
-    public class WeatherEntry
-    {
-        public string Date { get; set; }
-        public string MaxTemperature { get; set; }
-        public string MinTemperature { get; set; }
     }
 }
---
+++ WeatherParser/WeatherEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherParser
+{
+    public class WeatherEntry
+    {
+        public string Date { get; set; }
+        public string MaxTemperature { get; set; }
+        public string MinTemperature { get; set; }
+    }
+}
